073e39420822667a9f6294e7222f297917a6b481
AleWang16 <awang21913@gmail.com>
2025-11-19 01:03:48 -0500

    working on filtering for past 24 hours

--- src/webscraper/scrapers/pandascore/scraper.py
+++ src/webscraper/scrapers/pandascore/scraper.py
@@ -1,16 +1,19 @@
 """PandaScore scraper implementation for extracting match, team, and player data from PandaScore API."""
 from scrapers.base import BaseScraper
 from scrapers.pandascore.config import headers
+from datetime import datetime, timedelta, timezone
 import requests
-
 class PandaScoreScraper(BaseScraper):
     
     def __init__(self):
         self.base_url = "https://api.pandascore.co/csgo/"
-        self.filter = "?filter[videogame_title]=cs-2&page=1&per_page=1"
-    
+        self.base_filter = "?filter[videogame_title]=cs-2"
+        self.base_pagination = "&page=1&per_page=1"
     def scrape_matches(self):
-        url = self.base_url + 'matches' + self.filter
+        current_date = datetime.now(timezone.utc)
+        past_date = current_date - timedelta(hours=24)
+        url = self.base_url + 'matches' + self.base_filter + "&filter[begin_at][0]=" + past_date.isoformat() + "&filter[end_at][0]=" + current_date.isoformat() + self.base_pagination
+        print(url)
         response = requests.get(url, headers=headers)
         return response
 
@@ -26,5 +29,8 @@ class PandaScoreScraper(BaseScraper):
     def get_tournaments(self):
         pass
 
-# scraper = PandaScoreScraper()
-# print(scraper.scrape_matches().text)
+# print((datetime.now(timezone.utc) - timedelta(hours=24)).isoformat())
+# dt_str = datetime.now(timezone.utc) - timedelta(hours=24)
+# print(datetime.now(timezone.utc).isoformat())
+scraper = PandaScoreScraper()
+print(scraper.scrape_matches().text)
