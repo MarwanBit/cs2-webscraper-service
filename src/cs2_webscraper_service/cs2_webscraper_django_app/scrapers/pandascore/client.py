22adcfd3a75f9701e4f889a1226b4fc37493334e
Alejandro Wang <awang21913@gmail.com>
2026-01-21 21:13:31 -0500

    copied pandascore client

--- src/cs2_webscraper_service/cs2_webscraper_django_app/scrapers/pandascore/client.py
+++ src/cs2_webscraper_service/cs2_webscraper_django_app/scrapers/pandascore/client.py
@@ -1,33 +1,59 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+"""HTTP client for making authenticated requests to the PandaScore API with rate limiting."""
+from webscraper.scrapers.base import BaseClient
+from webscraper.scrapers.pandascore.config import headers
+from datetime import datetime, timedelta, timezone
+from webscraper.scrapers.pandascore.utils import convert_datetime_to_string
 import requests
+from webscraper.modules.teams_service import get_all_teams
 
-
-class PandascoreClient:
-
+class PandaScoreClient(BaseClient):
     def __init__(self):
-        self.base_url = "https://api.pandascore.co/"
-        # THIS NEEDS TO IMMEDIATELY BE REMOVED
-        self.pandascore_auth_token = "YOUR_PANDASCORE_API_KEY_HERE"
-        self.headers = {"Authorization": "Bearer " + self.pandascore_auth_token}
+        self.base_url = "https://api.pandascore.co/csgo/"
+        self.base_filter = "?filter[videogame_title]=cs-2"
+        self.base_pagination = "&page=1&per_page=1"
+    
+    # when scraping matches, what date range, params, live or historical?
+    # If historical, use rest api, if not use websockets
+    # take into account rate limits given
 
-    def get_matches(self):
-        r = requests.get(self.base_url + 'csgo/matches', headers=self.headers)
-        return r.json()
-
+    def scrape_matches(self):
+        teams = get_all_teams()
+        current_date = datetime.now(timezone.utc)
+        past_date = current_date - timedelta(hours=24)
+        for t in teams:
+            url = self.base_url + 'matches' + self.base_filter + "&filter[opponent_id]=" + t.team_name.lower() + "&range[begin_at]=" + convert_datetime_to_string(past_date) + "," + convert_datetime_to_string(current_date) + self.base_pagination
+            print(url)
+            response = requests.get(url, headers=headers)
+            print(response.text)
+        # add matches to the the db
+        # add teams to the db
+        # add tournaments to the db
+        return ""
+    
+    # have to figure out api response format to get the results
+    
     def get_results(self):
-        r = requests.get(self.base_url + 'csgo/matches/past', headers=self.headers)
-        return r.json()
+        # fetch leftover matches from db
+        matches = ["123", "456", "789"]
+        for match_id in matches:
+            url = self.base_url + "matches/" + match_id
+            response = requests.get(url, headers=headers)
+            # insert to results table in db
+
 
     def get_teams(self):
-        r = requests.get(self.base_url + 'csgo/teams', headers=self.headers)
-        return r.json()
-
-    # This seems to be a thing that we get strictly from HLTV, so we are going to have some work to do
+        pass
+    
     def get_team_rankings(self):
-        r = requests.get(self.base_url + '')
-        return ""
+        pass
 
     def get_tournaments(self):
-        r = requests.get(self.base_url + 'csgo/tournaments', headers=self.headers)
-        return r.json()
+        pass
+
+# print(datetime.now(timezone.utc).isoformat().replace('+00:00', 'Z'))
+# print((datetime.now(timezone.utc) - timedelta(hours=24)).isoformat().replace('+00:00', 'Z'))
+# dt_str = datetime.now(timezone.utc) - timedelta(hours=24)
+# print(datetime.now(timezone.utc).isoformat())
+client = PandaScoreClient()
+print(client.scrape_matches())
+
