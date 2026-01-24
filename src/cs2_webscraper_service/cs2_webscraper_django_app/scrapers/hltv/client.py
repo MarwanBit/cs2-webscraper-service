1f1bd39bdb2e6e51b3a65325f2124ad7ba2dfc21
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2026-01-24 02:06:59 -0500

    wrote get_match, get_results, get_team, get_team_ranking, get_tournament functions for HLTVClient

--- src/cs2_webscraper_service/cs2_webscraper_django_app/scrapers/hltv/client.py
+++ src/cs2_webscraper_service/cs2_webscraper_django_app/scrapers/hltv/client.py
@@ -8,7 +8,10 @@ from playwright.sync_api import sync_playwright
 class HLTVClient:
 
     def __init__(self):
+        #HLTV settings
         self.base_url = 'https://www.hltv.org'
+
+        # Browser settings
         self.browser_args = [
             "--disable-blink-features=AutomationControlled",
             "--no-sandbox",
@@ -22,51 +25,57 @@ class HLTVClient:
         self.viewport = {"width": 1280, "height": 800}
         self.locale = "en-US"
 
+        # timeouts
+        self.timeout = 60000
+
+        # Setting up the browser
+        self.playwright = sync_playwright().start()
+        self.browser = self.playwright.chromium.launch(headless=True, args=self.browser_args)
+        self.context = self.browser.new_context(
+            user_agent = self.user_agent,
+            viewport = self.viewport,
+            locale = self.locale
+        )
+        self.page = self.context.new_page()
+
+    def _fetch_page(self, url: str, selector: str, debug_file: str) -> str:
+        self.page.goto(url, wait_until="domcontentloaded")
+        self.page.wait_for_selector(selector, timeout = self.timeout)
+        html = self.page.content()
+        if debug_file:
+            with open(debug_file, "w", encoding="utf-8") as f:
+                f.write(html)
+        return html
 
     def get_match(self, match_id: str, match_name: str) -> HTTPResponse:
-        url = self.base_url + '/matches' + '/' + match_id + '/' + match_name
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True, args=self.browser_args)
-            context = browser.new_context(
-                user_agent = self.user_agent,
-                viewport = self.viewport,
-                locale = self.locale
-            )
-            page = context.new_page()
-            page.goto(url, wait_until="domcontentloaded")
-            page.wait_for_selector(".match-page", timeout=60000)
-            html = page.content()
-            with open("debug_hltv_matches.html", "w", encoding="utf-8") as f:
-                f.write(html)
-            browser.close()
-
+        url = f"{self.base_url}/matches/{match_id}/{match_name}"
+        return self._fetch_page(url, '.match-page', 'debug_hltv_matches.html')
+       
     def get_result(self, match_id: str, match_name: str) -> HTTPResponse:
         # same as get_match with some error handling
         pass
 
     def get_team(self, team_id: str, team_name: str) -> HTTPResponse:
-        url = self.base_url + '/team' + '/' + team_id + '/' + team_name
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True, args=self.browser_args)
-            context = browser.new_context(
-                user_agent = self.user_agent,
-                viewport = self.viewport,
-                locale = self.locale
-            )
-            page = context.new_page()
-            page.goto(url, wait_until="domcontentloaded")
-            page.wait_for_selector(".teamProfile", timeout=60000)
-            html = page.content()
-            with open("debug_hltv_team.html", "w", encoding="utf-8") as f:
-                f.write(html)
-            browser.close()
+        url = f"{self.base_url}/team/{team_id}/{team_name}"
+        return self._fetch_page(url, '.teamProfile', 'debug_hltv_team.html')
 
-    def get_team_ranking(self, team_id: str, date: datetime) -> HTTPResponse:
-        pass
+    def get_team_ranking(self, year: str, month: str, day: str, team_id: str, ranking_type: str) -> HTTPResponse:
+        # year is numerical: 2026
+        # month is lowercase: january, febuary, etc.
+        # day is numerial: 24
+        # ranking_type is one of : 'valve-ranking', 'ranking', their are others like regional but we don't support them
+        # regional addes another argument such as /ranking/teams/2026/january/19/country/United%States?teamid=11124
+        url = f"{self.base_url}/{ranking_type}/teams/{year}/{month}/{day}/?teamid={team_id}"
+        return self._fetch_page(url, '.hltv-logo-container', 'debug_htlv_tournament_ranking.html')
 
-    def get_tournament(self, tournament_id: str) -> HTTPResponse:
-        pass
+    def get_tournament(self, tournament_id: str, tournament_name: str) -> HTTPResponse:
+        url = f"{self.base_url}/events/{tournament_id}/{tournament_name}"
+        return self._fetch_page(url, '.event-hub', 'debug_hltv_tournament.html')
 
 if __name__ == "__main__":
     hltv_client = HLTVClient()
-    hltv_client.get_team('9565', 'vitality')
+    # hltv_client.get_match('2389280', 'paravision-vs-furia-blast-bounty-2026-season-1-finals')
+    # hltv_client.get_team('9565', 'vitality')
+    # hltv_client.get_tournament('8575', 'iem-krakw-2026-stage-1')
+    # this is vitality team_ranking (valve ranking)
+    hltv_client.get_team_ranking("2026", "january", '24', '95665', 'valve-ranking')
